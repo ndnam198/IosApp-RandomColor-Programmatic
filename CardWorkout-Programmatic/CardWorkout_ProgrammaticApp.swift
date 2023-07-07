bb436efd65b5add8542f456e67fa226e40bb31e4
ndnam198 <ndnam198@gmail.com>
2023-07-07 10:42:34 +0700

    Initial Commit

---
+++ CardWorkout-Programmatic/CardWorkout_ProgrammaticApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardWorkout_ProgrammaticApp.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Nam Nguyen on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CardWorkout_ProgrammaticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
